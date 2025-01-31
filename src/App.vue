0f54d369fd99936f242fc1ed76baf8e2d44de5c9
André Vis <52654095+neutron-cracker@users.noreply.github.com>
2025-01-31 17:25:41 +0100

    replicate website from last year

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,194 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+        <div class="bg-img"></div>
+        <div class="container-fluid">
+            <div class="row pr-5 pl-5 pt-5">
+                <div class="col-sm-6 align-self-center">
+                    <div class="row mb-3">
+                        <a class="gala-page-link" href="https://ichthusutrecht.nl">
+                            <i class="fas fa-arrow-left"></i>
+                            Terug naar de website
+                        </a>                 
+                    </div>
+                    <div class="row">
+                        <h1 class="gala-page-title">
+                            Welkom...<hr/>
+                        </h1>
+                    </div>
+                    <div class="row">
+                        <div class="gala-page-text text-justify">
+                            Van harte welkom op de website van het gala van 2024! Het feest zal plaatsvinden op 1 maart, aan het einde van de diesweek. Het thema is al eerder bekend gemaakt: 'Western Chique'!
+                            Verder is het, omdat het gala in een schrikkeljaar valt, dit jaar aan de dames om een brief te schrijven aan de heren. 
+                            <br>Vergeet niet om u en uw date aan te melden voor het gala met het formulier hieronder.
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+                            
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-sm">
+                            <a class="gala-page-link" href="https://www.instagram.com/galacie_ichthusutrecht/">
+                                <i class="fab fa-instagram pr-2"></i>
+                                Instagram
+                            </a>   
+                        </div>
+                        <div class="col-sm">
+                            <a class="gala-page-link" href="#" style="text-decoration:line-through;color:gray;">
+                                <i class="fab fa-youtube pr-2"></i>
+                                Introvideo
+                            </a>   
+                        </div>
+                        <div class="col-sm">
+                            <a class="gala-page-link" href="https://forms.gle/V3Yr6n9FvYy4f8MS7">
+                                <i class="fas fa-pencil-alt pr-2"></i>
+                                Inschrijven
+                            </a>   
+                        </div>                    
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div>
+                        <img class="img-fluid" src="./assets/gala24.png" alt="'"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row pt-5 ml-0">
+                <hr/>
+            </div>
+            <!--div class="row pt-5 ml-0">
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/E5wUzmVB-aQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div-->
+            <div class="row pt-5 ml-0">
+                <hr/>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row pl-5 pt-5 ml-0">
+                <div class="col-sm-6">
+                    <div class="row pr-5">
+                        <div class="row w-100">
+                            <h1 class="gala-page-title">
+                                Etiquette<hr/>
+                            </h1>
+                        </div>
+                        <div class="row w-100">
+                            <div class="gala-page-text">
+                                Om het geheugen weer eens goed op te frissen staan hieronder de etiquette nog eens goed uitgelegd.
+                            </div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="gala-page-header">
+                                Brief schrijven
+                            </div>
+                            <div class="gala-page-text">
+                            De dame vraagt de heer, middels een handgeschreven brief met gouden inkt op roze papier, of hij haar wil vergezellen op het gala. De heer antwoordt de dame met een handgeschreven brief met zilveren inkt op blauw papier. 
+                            Indien hij ingaat op haar verzoek, nodigt hij haar uit om een kopje thee te komen drinken. 
+                            </div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="gala-page-header w-100">
+                                Theedate
+                            </div>
+                            <div class="gala-page-text">
+                                Tijdens de visite wordt er niet over het gala gesproken. De heer laat door de versnapering bij de thee weten wat de dame kan verwachten:
+                                <ul>
+                                    <li>
+                                        Niets bij de thee: de dame kan niets verwachten.
+                                    </li>
+                                    <li>
+                                        Koekjes bij de thee: de dame mag met de heer naar het gala, maar mag daarna niets meer verwachten.
+                                    </li>
+                                    <li>
+                                        Zelfgebakken of gekochte taart (geen slagroomtaart): de heer is in meer dan alleen het gala geïnteresseerd, de dame moet hem na het gala veroveren.
+                                    </li>
+                                    <li>
+                                        Slagroomtaart: de dame heeft de heer al veroverd. 
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>  
+                        <div class="row pt-3">
+                            <div class="gala-page-header">
+                                Corsages
+                            </div>
+                            <div class="gala-page-text">
+                                Tijdens het gala is het gebruikelijk om corsages te dragen. Zo wordt duidelijk welke heer bij welke dame hoort. De dame verzorgt de corsages voor zowel haar galadate als zichzelf. De corsage mag de dame zelf bepalen, maar wel moet uit de corsage duidelijk worden hoe de dame en heer elkaar hebben ontmoet. Echter, mocht de dame geen creativiteit hebben en verkeren in tijdnood, dan kan zij altijd kiezen voor een bloem als corsage. De heer draagt de corsage altijd naar boven en de dame draagt de corsage naar beneden.
+                            </div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="gala-page-header">
+                                Begeleiden
+                            </div>
+                            <div class="gala-page-text">
+                                De heer haalt de dame bij haar huis op en begroet haar met een voorzichtige handkus.
+                                Wanneer zij samen lopen, loopt de dame aan de linkerzijde van de heer. De heer laat de
+                                dame altijd voorgaan, met uitzondering van de trap, om ongepaste blikken te voorkomen.
+                            </div>
+                        </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+                        <div class="row pt-3">
+                                <div class="gala-page-header w-100">
+                                    Dresscode
+                                </div>
+                                <div class="gala-page-text w-100">
+                                    De dresscode is 'white tie', dit houdt het volgende in:
+                                </div>
+                            <div class="row w-100 pt-3 pl-5">
+                                <div class="col-sm-6 pr-5">
+                                    <div class="gala-page-header">
+                                        Voor de heer
+                                    </div>
+                                    <div class="gala-page-text">
+                                        Een rokkostuum bestaande uit een zwarte pantalon met enkele of dubbele bies
+                                        (boord/streep aan de zijkant), een wit katoenen overhemd met hoge kraag, een wit piqué
+                                        vest en een zwarte jas die aan de onderkant schuin gesneden panden heeft en aan de
+                                        achterkant lang en in het midden gespleten is. Uiteraard hoort daar een witte strik bij. De
+                                        outfit wordt afgemaakt met zwarte lakschoenen zonder gaatjes.
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 pr-5">
+                                    <div class="gala-page-header">
+                                        Voor de dame
+                                    </div>
+                                    <div class="gala-page-text">
+                                        Een lange galajurk tot aan de hak. De jurk mag strapless of met een chique decolleté zijn,
+                                        de kleur mag de dame zelf bepalen. De jurk heeft geen mouwen. Het gebruik van lange
+                                        handschoenen is toegestaan en het dragen van sieraden wordt aangemoedigd. Indien er
+                                        handschoenen worden gedragen worden armbanden over de handschoenen gedragen en
+                                        ringen onder de handschoenen. De dame dient de handschoenen gedurende de hele avond
+                                        te dragen, bij het diner mag zij ze uitdoen.
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="row pt-3">
+                            <div class="gala-page-text">
+                                Tijdens het gala hebben zowel de dame als de heer weinig met zich mee. Een horloge
+                                dragen is ongepast. De heer draagt twee zakdoeken met zich mee, een voor hem en een
+                                voor de dame. De vrouw mag een klein handtasje meedragen. Een mobiele telefoon is
+                                toegestaan mits deze op stil staat.
+                            </div>
+                        </div>
+                        <div class="w-100 pt-5"></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 text-center">
+                    <img id="gala-etiquette-sticky" src="./assets/gala24.png" alt="'"/>
+                </div>
+            </div>
+            <div class="row w-100 pt-3 pb-3">
+                <div class="gala-page-footer w-100 text-center">
+                    Vragen? Stuur de galacie een mailtje op <a class="gala-page-link" href="mailto:galacie.ichthusutrecht@gmail.com">galacie.ichthusutrecht@gmail.com</a>!
+                </div>
+            </div>
+        </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Import own style later, so it has the highest priority
+import './assets/main.css'
+
 
 createApp(App).mount('#app')
