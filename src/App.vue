10e4db4dfc6bb9b6eeab8738bbd270ac9fedc7cd
André Vis <52654095+neutron-cracker@users.noreply.github.com>
2025-02-02 16:42:13 +0100

    style page

--- src/App.vue
+++ src/App.vue
@@ -2,126 +2,136 @@
 </script>
 
 <template>
-  <div class="flex justify-center bg-[var(--gala-dark-red)]">
-    <div class="w-full max-w-3xl">
-      <div class="p-4 w-full">
-        <a href="https://ichthusutrecht.nl" class="text-[var(--gala-gold-dark)]">
-          <i class="fas fa-arrow-left"></i>
-          Terug naar de website
-        </a>
-        <div class="mt-4 text-white">
-          Van harte welkom op de website van het gala van 2025! Het feest zal plaatsvinden op 28 februari, aan het einde
-          van de lustrummaand. Het thema is al eerder bekend gemaakt: 'Casino Royale'!
-          <br>Vergeet niet om u en uw date aan te melden voor het gala met het formulier hieronder.
-        </div>
-        <div class="mt-4 text-[var(--gala-gold-light)]">
-          link aanmeldformulier
-        </div>
-
-        <!-- Uitleg Etiquette -->
-        <div class="mt-8">
-          <h1 class="text-[var(--gala-gold-dark)] text-2xl font-bold">
-            Etiquette
-          </h1>
-          <div class="text-[var(--gala-gold-light)] mt-2">
-            Om het geheugen weer eens goed op te frissen staan hieronder de etiquette nog eens goed uitgelegd.
+  <div class="bg-gala-dark-red min-h-screen space-y-8">
+    <img src="@/assets/gala-top.png" alt="Gala Top" class="w-min-full min-h-16 object-cover">
+    <div class="flex justify-center font-playfair-display italic font-medium">
+      <div class="w-full max-w-3xl">
+        <div class="p-4 w-full">
+          <a href="https://ichthusutrecht.nl" class="text-gala-gold-medium font-semibold">
+            <i class="fas fa-arrow-left"></i>
+            Terug naar de website
+          </a>
+          <div class="mt-4 text-gala-gold-light">
+            Van harte welkom op de website van het gala van 2025! Het feest zal plaatsvinden op 28 februari, aan het
+            einde
+            van de lustrummaand. Het thema is al eerder bekend gemaakt: 'Casino Royale'!
+            <br>Vergeet niet om u en uw date aan te melden voor het gala met het formulier hieronder.
           </div>
-          <div class="mt-4 space-y-6">
-            <div>
-              <div class="text-[var(--gala-gold-dark)] font-semibold">
-                Brief schrijven
-              </div>
-              <div class="text-[var(--gala-gold-light)] mt-2">
-                De heer vraagt de dame, middels een handgeschreven brief met zilveren inkt op
-                blauw papier, of zij zijn wil
-                vergezellen op het gala. De dame antwoordt de heer met een handgeschreven brief met gouden inkt op roze papier.
-                Indien hij ingaat op zijn verzoek, nodigt hij haar uit om een kopje thee te komen drinken.
-              </div>
+          <div class="mt-4 text-gala-gold-medium font-semibold">
+            link aanmeldformulier
+          </div>
+
+          <!-- Uitleg Etiquette -->
+          <div class="mt-8">
+            <h1 class="text-gala-gold-dark text-2xl font-bold">
+              Etiquette
+            </h1>
+            <div class="text-gala-gold-light mt-2">
+              Om het geheugen weer eens goed op te frissen staan hieronder de etiquette nog eens goed uitgelegd.
             </div>
-            <div>
-              <div class="text-[var(--gala-gold-dark)] font-semibold">
-                Theedate
-              </div>
-              <div class="text-[var(--gala-gold-light)] mt-2">
-                Tijdens de visite wordt er niet over het gala gesproken. De dame laat door de versnapering bij de thee
-                weten wat de heer kan verwachten:
-                <ul class="list-disc list-inside mt-2">
-                  <li>Niets bij de thee: de heer kan niets verwachten.</li>
-                  <li>Koekjes bij de thee: de heer mag met de dame naar het gala, maar mag daarna niets meer verwachten.</li>
-                  <li>Zelfgebakken of gekochte taart (geen slagroomtaart): de dame is in meer dan alleen het gala geïnteresseerd,
-                     de heer moet haar na het gala veroveren.</li>
-                  <li>Slagroomtaart: de heer heeft de dame al veroverd.</li>
-                </ul>
-              </div>
-            </div>
-            <div>
-              <div class="text-[var(--gala-gold-dark)] font-semibold">
-                Corsages
-              </div>
-              <div class="text-[var(--gala-gold-light)] mt-2">
-                Tijdens het gala is het gebruikelijk om corsages te dragen. Zo wordt duidelijk welke dame bij welke heer
-                hoort. De heer verzorgt de corsages voor zowel haar galadate
-                 als zichzelf. De corsage mag de heer zelf
-                bepalen, maar wel moet uit de corsage duidelijk worden hoe de heer en dame elkaar hebben ontmoet.
-                Echter, mocht de heer geen creativiteit hebben en verkeren in tijdnood, dan kan zij altijd kiezen voor een bloem
-                als corsage. De dame draagt de corsage altijd naar boven en de heer draagt de corsage naar beneden.
-              </div>
-            </div>
-            <div>
-              <div class="text-[var(--gala-gold-dark)] font-semibold">
-                Begeleiden
-              </div>
-              <div class="text-[var(--gala-gold-light)] mt-2">
-                De heer haalt de dame bij haar huis op en begroet haar met een voorzichtige handkus.
-                Wanneer zij samen lopen, loopt de heer aan de linkerzijde van de dame. De dame laat de
-                heer altijd voorgaan, met uitzondering van de trap, om ongepaste blikken te voorkomen.
-              </div>
-            </div>
-            <div>
-              <div class="text-[var(--gala-gold-dark)] font-semibold">
-                Dresscode
-              </div>
-              <div class="text-[var(--gala-gold-light)] mt-2">
-                De dresscode is 'white tie', dit houdt het volgende in:
-              </div>
-              <div class="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                  <div class="text-[var(--gala-gold-dark)] font-semibold">
-                    Voor de heer
-                  </div>
-                  <div class="text-[var(--gala-gold-light)] mt-2">
-                    Een rokkostuum bestaande uit een zwarte pantalon met enkele of dubbele bies
-                    (boord/streep aan de zijkant), een wit katoenen overhemd met hoge kraag, een wit piqué
-                    vest en een zwarte jas die aan de onderkant schuin gesneden panden heeft en aan de
-                    achterkant lang en in het midden gespleten is. Uiteraard hoort daar een witte strik bij. De
-                    outfit wordt afgemaakt met zwarte lakschoenen zonder gaatjes.
-                  </div>
+            <div class="mt-4 space-y-6">
+              <div>
+                <div class="text-gala-gold-medium font-semibold">
+                  Brief schrijven
                 </div>
-                <div>
-                  <div class="text-[var(--gala-gold-dark)] font-semibold">
-                    Voor de dame
-                  </div>
-                  <div class="text-[var(--gala-gold-light)] mt-2">
-                    Een lange galajurk tot aan de hak. De jurk mag strapless of met een chique decolleté zijn,
-                    de kleur mag de dame zelf bepalen. De jurk heeft geen mouwen. Het gebruik van lange
-                    handschoenen is toegestaan en het dragen van sieraden wordt aangemoedigd. Indien er
-                    handschoenen worden gedragen worden armbanden over de handschoenen gedragen en
-                    ringen onder de handschoenen. De dame dient de handschoenen gedurende de hele avond
-                    te dragen, bij het diner mag zij ze uitdoen.
-                  </div>
+                <div class="text-gala-gold-light mt-2">
+                  De heer vraagt de dame, middels een handgeschreven brief met zilveren inkt op
+                  blauw papier, of zij zijn wil
+                  vergezellen op het gala. De dame antwoordt de heer met een handgeschreven brief met gouden inkt op
+                  roze papier.
+                  Indien hij ingaat op zijn verzoek, nodigt hij haar uit om een kopje thee te komen drinken.
                 </div>
               </div>
-              <div class="text-[var(--gala-gold-light)] mt-4">
-                Tijdens het gala hebben zowel de heer als de dame weinig met zich mee. Een horloge
-                dragen is ongepast. De heer draagt twee zakdoeken met zich mee, een voor hem en een
-                voor de dame. De vrouw mag een klein handtasje meedragen. Een mobiele telefoon is
-                toegestaan mits deze op stil staat.
+              <div>
+                <div class="text-gala-gold-medium font-semibold">
+                  Theedate
+                </div>
+                <div class="text-gala-gold-light mt-2">
+                  Tijdens de visite wordt er niet over het gala gesproken. De dame laat door de versnapering bij de thee
+                  weten wat de heer kan verwachten:
+                  <ul class="list-disc list-inside mt-2">
+                    <li>Niets bij de thee: de heer kan niets verwachten.</li>
+                    <li>Koekjes bij de thee: de heer mag met de dame naar het gala, maar mag daarna niets meer
+                      verwachten.</li>
+                    <li>Zelfgebakken of gekochte taart (geen slagroomtaart): de dame is in meer dan alleen het gala
+                      geïnteresseerd,
+                      de heer moet haar na het gala veroveren.</li>
+                    <li>Slagroomtaart: de heer heeft de dame al veroverd.</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div class="text-gala-gold-medium font-semibold">
+                  Corsages
+                </div>
+                <div class="text-gala-gold-light mt-2">
+                  Tijdens het gala is het gebruikelijk om corsages te dragen. Zo wordt duidelijk welke dame bij welke
+                  heer
+                  hoort. De heer verzorgt de corsages voor zowel haar galadate
+                  als zichzelf. De corsage mag de heer zelf
+                  bepalen, maar wel moet uit de corsage duidelijk worden hoe de heer en dame elkaar hebben ontmoet.
+                  Echter, mocht de heer geen creativiteit hebben en verkeren in tijdnood, dan kan zij altijd kiezen voor
+                  een bloem
+                  als corsage. De dame draagt de corsage altijd naar boven en de heer draagt de corsage naar beneden.
+                </div>
+              </div>
+              <div>
+                <div class="text-gala-gold-medium font-semibold">
+                  Begeleiden
+                </div>
+                <div class="text-gala-gold-light mt-2">
+                  De heer haalt de dame bij haar huis op en begroet haar met een voorzichtige handkus.
+                  Wanneer zij samen lopen, loopt de heer aan de linkerzijde van de dame. De dame laat de
+                  heer altijd voorgaan, met uitzondering van de trap, om ongepaste blikken te voorkomen.
+                </div>
+              </div>
+              <div>
+                <div class="text-gala-gold-medium font-semibold">
+                  Dresscode
+                </div>
+                <div class="text-gala-gold-light mt-2">
+                  De dresscode is 'white tie', dit houdt het volgende in:
+                </div>
+                <div class="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <div class="text-gala-gold-medium font-semibold">
+                      Voor de heer
+                    </div>
+                    <div class="text-gala-gold-light mt-2">
+                      Een rokkostuum bestaande uit een zwarte pantalon met enkele of dubbele bies
+                      (boord/streep aan de zijkant), een wit katoenen overhemd met hoge kraag, een wit piqué
+                      vest en een zwarte jas die aan de onderkant schuin gesneden panden heeft en aan de
+                      achterkant lang en in het midden gespleten is. Uiteraard hoort daar een witte strik bij. De
+                      outfit wordt afgemaakt met zwarte lakschoenen zonder gaatjes.
+                    </div>
+                  </div>
+                  <div>
+                    <div class="text-gala-gold-medium font-semibold">
+                      Voor de dame
+                    </div>
+                    <div class="text-gala-gold-light mt-2">
+                      Een lange galajurk tot aan de hak. De jurk mag strapless of met een chique decolleté zijn,
+                      de kleur mag de dame zelf bepalen. De jurk heeft geen mouwen. Het gebruik van lange
+                      handschoenen is toegestaan en het dragen van sieraden wordt aangemoedigd. Indien er
+                      handschoenen worden gedragen worden armbanden over de handschoenen gedragen en
+                      ringen onder de handschoenen. De dame dient de handschoenen gedurende de hele avond
+                      te dragen, bij het diner mag zij ze uitdoen.
+                    </div>
+                  </div>
+                </div>
+                <div class="text-gala-gold-light mt-4">
+                  Tijdens het gala hebben zowel de heer als de dame weinig met zich mee. Een horloge
+                  dragen is ongepast. De heer draagt twee zakdoeken met zich mee, een voor hem en een
+                  voor de dame. De vrouw mag een klein handtasje meedragen. Een mobiele telefoon is
+                  toegestaan mits deze op stil staat.
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <img src="@/assets/gala-bottom.png" alt="Gala Bottom" class="w-min-full min-h-16 object-cover">
   </div>
 </template>
 
